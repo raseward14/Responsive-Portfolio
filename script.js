fee53ce4743244cdaf7588f977bbfba9f6e4e8e0
Richard Seward <raseward14@gmail.com>
2020-12-22 19:56:47 -0700

    added 768px breakpoint and nav hamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 $('.carousel').carousel({
     interval: 2000
   })
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-link');
+  const navLink = document.querySelectorAll('.nav-link li');
+ 
+  burger.addEventListener('click',()=>{
+      //Toggle Nav
+      nav.classList.toggle('nav-active');
+    
+      //Animate Links
+      navLink.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
+        }
+      });
+  });
+}
+
+navSlide();
+
+
