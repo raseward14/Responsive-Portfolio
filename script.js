10594f1559f42295f647e9a4775d9c6f733b3be4
Richard Seward <raseward14@gmail.com>
2020-12-23 20:44:08 -0700

    cleaned up javascript removing all code not needed for toggle navbar

--- script.js
+++ script.js
@@ -10,15 +10,6 @@ const navSlide = () => {
   burger.addEventListener('click',()=>{
       //Toggle Nav
       nav.classList.toggle('nav-active');
-    
-      //Animate Links
-      navLink.forEach((link, index) => {
-        if (link.style.animation) {
-            link.style.animation = '';
-        } else {
-            link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
-        }
-      });
   });
 }
 
