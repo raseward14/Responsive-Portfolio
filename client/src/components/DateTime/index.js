d41d00b443004213b74d6c38713e302c7885779a
Richard Seward <raseward14@gmail.com>
2021-11-20 13:46:58 -0700

    changed font to Dekko, and made the date and time responsive with DateTime media queries, changing width and font size

--- client/src/components/DateTime/index.js
+++ client/src/components/DateTime/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
-import Ink from '../Ink'
 
 export const DateTime = () => {
     var [date, setDate] = useState(new Date());
