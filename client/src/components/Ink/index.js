6bd205b97e1d5e261ee5ff11b96fa5e3c9c91757
Richard Seward <raseward14@gmail.com>
2021-11-24 07:44:26 -0700

    removed inline style making the icon bg white, fa-circle class inherits its parents bg color

--- client/src/components/Ink/index.js
+++ client/src/components/Ink/index.js
@@ -37,7 +37,7 @@ const Ink = ({ setBackgroundColor }) => {
 
     return (
         <div className="dropdown" data-dropdown>
-            <button onClick={inkFunction} className="dropbtn"><FontAwesomeIcon className="fa-circle" icon={faEyeDropper} style={{ backgroundColor: 'white' }} data-dropdown-button /></button>
+            <button onClick={inkFunction} className="dropbtn"><FontAwesomeIcon className="fa-circle" icon={faEyeDropper} data-dropdown-button /></button>
             <div id="inkDropdown" className="dropdown-content">
                 {colors.map((color, key) => {
                     return (
