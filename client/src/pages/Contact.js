d17ceebc97bb2b1e04ef305bd22de0cbb82cbbf2
Richard Seward <raseward14@gmail.com>
2021-12-09 08:04:32 -0700

    DEV-183[in progress]

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -33,7 +33,62 @@ function Contact() {
         setFormObject({ ...formObject, [name]: value });
     }
 
-    function handlFormSubmit(event) {
+    function postClickUp() {
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "pk_14917287_C01GVS3FA1OD9YG1CWV64YI515GS09QD");
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "name": formObject.name,
+            "description": "New contact, follow up with email!",
+            "assignees": [
+                183
+            ],
+            "tags": [
+                "contact"
+            ],
+            "status": "To do",
+            "priority": null,
+            "due_date": 1508369194377,
+            "due_date_time": false,
+            "time_estimate": 8640000,
+            "start_date": 1567780450202,
+            "start_date_time": false,
+            "notify_all": true,
+            "parent": null,
+            "links_to": null,
+            "check_required_custom_fields": true,
+            "custom_fields": [
+                {
+                    "id": "041c6ec7-59d6-45c7-9cad-67f54d677be4",
+                    "value": formObject.email
+                },
+                {
+                    "id": "546bbe4b-2fa6-49e9-bb7c-3c111aec15eb",
+                    "value": formObject.message
+                },
+                {
+                    "id": "865c7fe2-d4c9-4464-9d71-cc4d190eab98",
+                    "value": `+1 ${formObject.phone}`
+                }
+            ]
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            mode: 'no-cors',
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("https://api.clickup.com/api/v2/list/116273262/task", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    };
+
+    function handleFormSubmit(event) {
         event.preventDefault();
         if (formObject.name && formObject.phone || formObject.email) {
             contactAPIFunctions
@@ -82,7 +137,7 @@ function Contact() {
                 <FormBtn
                     id='Button'
                     disabled={!(formObject.name && formObject.message && (formObject.phone || formObject.email))}
-                    onClick={handlFormSubmit}
+                    onClick={handleFormSubmit}
                 >{buttonText}</FormBtn>
             </section>
         </>
