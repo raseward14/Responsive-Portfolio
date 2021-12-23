bf0f34d884c72142ae0b2062d11a349f96127b96
Richard Seward <raseward14@gmail.com>
2021-12-23 11:02:07 -0700

    added text align prop to class home text and mern icons, also gave mern icons a unique class name so they can be styled independent of other styles

--- client/src/components/MERNfooter/index.js
+++ client/src/components/MERNfooter/index.js
@@ -7,11 +7,11 @@ import { faReact, faNodeJs } from '@fortawesome/free-brands-svg-icons'
 function MERN() {
     return (
         <div className='container'>
-            <div className='wrapper'>
+            <div className='icon-wrapper'>
                 <img className='one mern-icon' src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/000000/external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo.png"/>
                 <img className='two mern-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
-                <FontAwesomeIcon className='three' icon={faReact} />
-                <FontAwesomeIcon className='four' icon={faNodeJs} />
+                <FontAwesomeIcon className='three mern-icon' icon={faReact} />
+                <FontAwesomeIcon className='four mern-icon' icon={faNodeJs} />
 
             </div>
         </div>
