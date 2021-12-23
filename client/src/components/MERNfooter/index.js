a7dc275c41e2a24bd8788c677ef37d28f1a6ec16
Richard Seward <raseward14@gmail.com>
2021-12-23 13:34:51 -0700

    page body position relative and mern-icon container position absolute bottom 0, removed relative position from section, also in media query, and from class home

--- client/src/components/MERNfooter/index.js
+++ client/src/components/MERNfooter/index.js
@@ -6,7 +6,7 @@ import { faReact, faNodeJs } from '@fortawesome/free-brands-svg-icons'
 
 function MERN() {
     return (
-        <div className='container'>
+        <div className='icon-container'>
             <div className='icon-wrapper'>
                 <img className='one mern-icon' src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/000000/external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo.png"/>
                 <img className='two mern-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
