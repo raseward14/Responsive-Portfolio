17a34daa42ebea8ee28e0ae450040c0461e3a82b
richardseward <raseward14@gmail.com>
2023-12-25 19:38:59 -0700

    added switch to dynamically determine border hover class

--- client/src/components/ProjectFilters/index.js
+++ client/src/components/ProjectFilters/index.js
@@ -3,9 +3,10 @@ import './style.css';
 
 const ProjectFilters = ({ backgroundColor }) => {
   const [activeTab, setActiveTab] = useState('all');
+  const tabs = ['all', 'react.js', 'relational', 'non-relational'];
 
   const handleClick = (tab) => {
-    setActiveTab(tab)
+    setActiveTab(tab);
   };
 
   useEffect(() => {
@@ -14,7 +15,179 @@ const ProjectFilters = ({ backgroundColor }) => {
 
   return (
     <div className="filter-wrapper">
-      {activeTab === 'all' ? 
+      {tabs.map((value) => {
+        switch (backgroundColor) {
+          case '#7BDCB5' && activeTab === value:
+            <div
+              className="filter-tab teal"
+              style={{ 'background-color': backgroundColor }}
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case ('#7BDCB5' && activeTab !== value):
+            <div
+              className="filter-tab teal"
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#00D084' && activeTab === value:
+            <div
+              className="filter-tab green"
+              style={{ 'background-color': backgroundColor }}
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#00D084' && activeTab !== value:
+            <div
+              className="filter-tab green"
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#8ED1FC' && activeTab === value:
+            <div
+              className="filter-tab light-blue"
+              style={{ 'background-color': backgroundColor }}
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#8ED1FC' && activeTab !== value:
+            <div
+              className="filter-tab light-blue"
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#0693E3' && activeTab === value:
+            <div
+              className="filter-tab blue"
+              style={{ 'background-color': backgroundColor }}
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#0693E3' && activeTab !== value:
+            <div
+              className="filter-tab blue"
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#ABB8C3' && activeTab === value:
+            <div
+              className="filter-tab gray"
+              style={{ 'background-color': backgroundColor }}
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#ABB8C3' && activeTab !== value:
+            <div
+              className="filter-tab gray"
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#EB144C' && activeTab === value:
+            <div
+              className="filter-tab red"
+              style={{ 'background-color': backgroundColor }}
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#EB144C' && activeTab !== value:
+            <div className="filter-tab red" onClick={() => setActiveTab(value)}>
+              {value}
+            </div>;
+            break;
+          case '#F78DA7' && activeTab === value:
+            <div
+              className="filter-tab pink"
+              style={{ 'background-color': backgroundColor }}
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#F78DA7' && activeTab !== value:
+            <div
+              className="filter-tab pink"
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#9900EF' && activeTab === value:
+            <div
+              className="filter-tab indigo"
+              style={{ 'background-color': backgroundColor }}
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#9900EF' && activeTab !== value:
+            <div
+              className="filter-tab indigo"
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#05F725' && activeTab === value:
+            <div
+              className="filter-tab lime"
+              style={{ 'background-color': backgroundColor }}
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#05F725' && activeTab !== value:
+            <div
+              className="filter-tab lime"
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#FCB900' && activeTab === value:
+            <div
+              className="filter-tab orange"
+              style={{ 'background-color': backgroundColor }}
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+          case '#FCB900' && activeTab !== value:
+            <div
+              className="filter-tab orange"
+              onClick={() => setActiveTab(value)}
+            >
+              {value}
+            </div>;
+            break;
+        }
+      })}
+
+      {/* {activeTab === 'all' ? 
       <div
       className="filter-tab"
       style={{'background-color': backgroundColor}}
@@ -47,7 +220,7 @@ const ProjectFilters = ({ backgroundColor }) => {
       :      
       <div className="filter-tab"
       onClick={()=> setActiveTab('non-relational')}>non-relational</div>
-      }
+      } */}
     </div>
   );
 };
