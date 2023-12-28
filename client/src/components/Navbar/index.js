17b41c9849c0d393744fcf83d8b266d08827a665
Richard Seward <raseward14@gmail.com>
2023-12-27 17:46:42 -0700

    removed font size change on navLink hover and added a default white border so the elements dont jump when a colored border is added

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,7 +4,8 @@ import { Nav, NavItem, NavLink } from "reactstrap";
 
 import Icons from "../Icons";
 
-function Navbar() {
+function Navbar({ backgroundColor }) {
+    const [hoverStyle, setHoverStyle] = useState('orange');
     // When the user clicks the hamburger, toggle between hiding and showing the navbar
     // const menuBtn = document.querySelector('.menu-btn');
     // let menuOpen = false;
@@ -17,6 +18,43 @@ function Navbar() {
     const hamburgerFunction = () => {
         document.getElementById("navMenu").classList.toggle("show");
     };
+
+    useEffect(() => {
+        switch (backgroundColor) {
+            case '#7BDCB5':
+              setHoverStyle('teal-button');
+              break;
+            case '#00D084':
+              setHoverStyle('green-button');
+              break;
+            case '#8ED1FC':
+              setHoverStyle('light-blue-button');
+              break;
+            case '#0693E3':
+              setHoverStyle('blue-button');
+              break;
+            case '#ABB8C3':
+              setHoverStyle('gray-button');
+              break;
+            case '#EB144C':
+              setHoverStyle('red-button');
+              break;
+            case '#F78DA7':
+              setHoverStyle('pink-button');
+              break;
+            case '#9900EF':
+              setHoverStyle('indigo-button');
+              break;
+            case '#05F725':
+              setHoverStyle('lime-button');
+              break;
+            default:
+              setHoverStyle('orange-button');
+              break;
+          }
+          console.log(`from nav component ${backgroundColor}`);
+    }, [backgroundColor]);
+
     return (
         <div className='dropdown1'>
             <div className='menu-button' onClick={hamburgerFunction}>
@@ -25,19 +63,19 @@ function Navbar() {
             <div id='navMenu' className="navbar-content">
                 <Nav className="nav-bar-main">
                     <NavItem>
-                        <NavLink href="/" active>Home</NavLink>
+                        <NavLink className={`${hoverStyle}`} href="/" active>Home</NavLink>
                     </NavItem>
 
                     <NavItem>
-                        <NavLink href="about">About</NavLink>
+                        <NavLink className={`${hoverStyle}`} href="about">About</NavLink>
                     </NavItem>
 
                     <NavItem className="float-right">
-                        <NavLink href="contact">Contact</NavLink>
+                        <NavLink className={`${hoverStyle}`} href="contact">Contact</NavLink>
                     </NavItem>
 
                     <NavItem className="float-right">
-                        <NavLink href="portfolio">Portfolio</NavLink>
+                        <NavLink className={`${hoverStyle}`} href="portfolio">Portfolio</NavLink>
                     </NavItem>
                 </Nav>
                 <Icons />
